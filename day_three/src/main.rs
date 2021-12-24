31205d0ffae2e00c46e74f081f40e00f34b45c33
Fletcher Porter <me@fletcherporter.com>
2021-12-23 17:36:20 -0800

    Finish day 3 part 2 with Rust

--- day_three/src/main.rs
+++ day_three/src/main.rs
@@ -2,19 +2,67 @@ fn main() {
     let (diagnostic_data, number_of_bits, majority_threshold) =
         diagnostic_stuff_from_file("binary_diagnostic.data");
 
-    let power_consumption = power_consumption(diagnostic_data, number_of_bits, majority_threshold);
-
-    println!("{:}", power_consumption);
+    let power_consumption = power_consumption(&diagnostic_data, number_of_bits, majority_threshold);
+    let life_support_rating = life_support_rating(&diagnostic_data, number_of_bits);
 
     assert_eq!(power_consumption, 4138664);
+    assert_eq!(life_support_rating, 4273224);
+
+    println!("{:}", power_consumption);
+    println!("{:}", life_support_rating);
 }
 
-fn oxygen_generator_rating(diagnostic_data: Vec<u32>, number_of_bits: usize) -> u32 {
-    0
+fn life_support_rating(diagnostic_data: &Vec<u32>, number_of_bits: usize) -> u32 {
+    return oxygen_generator_rating(diagnostic_data, number_of_bits) * co2_scrubber_rating(diagnostic_data, number_of_bits)
+}
+
+fn oxygen_generator_rating(diagnostic_data: &Vec<u32>, number_of_bits: usize) -> u32 {
+    return system_rating(diagnostic_data, number_of_bits, 1)
+}
+
+fn co2_scrubber_rating(diagnostic_data: &Vec<u32>, number_of_bits: usize) -> u32 {
+    return system_rating(diagnostic_data, number_of_bits, 0)
+}
+
+fn system_rating(diagnostic_data: &Vec<u32>, number_of_bits: usize, on_majority_1s: u32) -> u32 {
+    assert!(on_majority_1s == 0 || on_majority_1s == 1);
+    let on_majority_0s: u32 = if on_majority_1s == 0 { 1 } else { 0 };
+
+    let mut data: Vec<u32> = diagnostic_data.clone();
+    for i in 0..number_of_bits {
+        let current_bit: u32 = (number_of_bits - i - 1) as u32;
+
+        let data_length: usize = data.len();
+
+        if data_length == 1 {
+            break;
+        }
+        if data_length == 0 {
+            panic!("All data got filtered out on bit {:}", i - 1);
+        }
+
+        // determine whether 0 or 1 is the least common ith bit
+        let number_of_1s: usize = data
+            .iter()
+            .filter(|&&datum| datum & (1 << current_bit) != 0)
+            .count();
+
+        let least_common_bit: u32 = (
+            if number_of_1s >= (data_length / 2) { on_majority_1s } else { on_majority_0s }
+        ) << current_bit;
+
+        // select those data which have the least common ith bit
+        data = data
+            .iter()
+            .filter(|&&datum| datum & (1 << current_bit) == least_common_bit)
+            .copied()
+            .collect();
+    }
+    return data[0]
 }
 
 fn power_consumption(
-    diagnostic_data: Vec<u32>,
+    diagnostic_data: &Vec<u32>,
     number_of_bits: usize,
     majority_threshold: u32,
 ) -> u32 {
@@ -34,7 +82,7 @@ fn power_consumption(
     gamma_rate * epsilon_rate
 }
 
-fn bit_counts(diagnostic_data: Vec<u32>, number_of_bits: usize) -> Vec<u32> {
+fn bit_counts(diagnostic_data: &Vec<u32>, number_of_bits: usize) -> Vec<u32> {
     let mut bit_counts: Vec<u32> = vec![0; number_of_bits];
     for datum in diagnostic_data.iter() {
         for i in 0..(number_of_bits) {
